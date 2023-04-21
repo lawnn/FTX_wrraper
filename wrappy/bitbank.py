ba8dd01a50c2a3275899ac5d4a18d21ff9971815
lawnn <bloom.all.over.12@gmail.com>
2023-04-21 22:22:37 +0900

    BitBank: Add Requests Orders

--- wrappy/bitbank.py
+++ wrappy/bitbank.py
@@ -25,3 +25,33 @@ class BitBank(BotBase):
                     "https://github.com/bitbankinc/bitbank-api-docs/blob/master/errors_JP.md")
             else:
                 return data["data"]
+
+    async def _replace_order(self, side: str, size: float, order_type: str, price: any = None, post_only: bool = False):
+        request = {
+            "pair": self.symbol,
+            "amount": size,
+            "side": side,
+            "type": order_type,
+            "post_only": post_only
+            }
+
+        if order_type == "limit":
+            request["price"] = price
+
+        return await self._requests('POST', url="/user/spot/order", data=request)
+
+    async def market_order(self, side: str, size: float):
+        try:
+            return await self._replace_order(side, size, "market")
+        except Exception as e:
+            self.log_error("API request failed in market_order.")
+            self.log_error(format_exc())
+            raise e
+
+    async def limit_order(self, side: str, size: float, price: any, post_only: bool = False):
+        try:
+            return await self._replace_order(side=side, size=size, order_type="limit", price=price, post_only=post_only)
+        except Exception as e:
+            self.log_error("API request failed in limit_order.")
+            self.log_error(format_exc())
+            raise e
