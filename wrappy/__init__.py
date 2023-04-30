b575810ac3c23754af9ae430032798852784bfc3
lawnn <bloom.all.over.12@gmail.com>
2023-05-01 03:42:21 +0900

    docstringsを追加とwith構文での実装とclean up

--- wrappy/__init__.py
+++ wrappy/__init__.py
@@ -1,7 +1,7 @@
 from .base import Notify, BotBase
 from .gmo import GMO
 from .bitbank import BitBank
-from .logfile import History
+from .logfile import OrderHistory
 from .exceptions import APIException
 from .util import *
 from .time_util import now_jst, now_jst_str, now_utc, now_utc_str, now_gmt, now_gmt_str
