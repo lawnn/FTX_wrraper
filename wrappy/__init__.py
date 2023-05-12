d403b4878863ffea18c297948b4ceff2ef6af2cd
lawnn <bloom.all.over.12@gmail.com>
2023-05-12 16:19:36 +0900

    Request Exceptionを追加

--- wrappy/__init__.py
+++ wrappy/__init__.py
@@ -3,6 +3,6 @@ from .gmo import GMO
 from .bitbank import BitBank
 from .coincheck import CoinCheck
 from .logfile import OrderHistory
-from .exceptions import APIException
+from .exceptions import APIException, RequestException
 from .util import *
 from .time_util import now_jst, now_jst_str, now_utc, now_utc_str, now_gmt, now_gmt_str
