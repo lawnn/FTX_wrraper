2eea8e5c5c140ebf6457c2992cb1080f98ce800b
lawnn <bloom.all.over.12@gmail.com>
2023-07-16 17:07:07 +0900

    ISOフォーマット変換

--- wrappy/__init__.py
+++ wrappy/__init__.py
@@ -6,4 +6,4 @@ from .coincheck import CoinCheck
 from .logfile import OrderHistory
 from .exceptions import APIException, RequestException
 from .util import *
-from .time_util import now_jst, now_jst_str, now_utc, now_utc_str, now_gmt, now_gmt_str
+from .time_util import now_jst, now_jst_str, now_utc, now_utc_str, now_gmt, now_gmt_str, fromISOformat
