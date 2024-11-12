dbacd56d7698a01d81ad2677e82028b0086041a9
lawnn <bloom.all.over.12@gmail.com>
2024-11-12 13:42:43 +0000

    Ver 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='wrappy',
     packages=['wrappy'],
-    version='0.4.1',
+    version='0.5.0',
     author='lawn',
     url='https://github.com/lawnn/wrappy.git',
     install_requires=['requests', 'asyncio', 'pybotters', 'matplotlib', 'pandas', 'numpy', 'polars', 'pytz']
